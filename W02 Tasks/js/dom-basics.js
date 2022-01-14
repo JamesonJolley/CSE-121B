7e7078ffeef1c5655561596a953e3ca209219a4e
JamesonJolley <jol21004@byui.edu>
2022-01-14 08:03:33 -0700

    stuff for w2

---
+++ W02 Tasks/js/dom-basics.js
@@ -0,0 +1,18 @@
+
+const newParagraph = document.createElement("p");
+newParagraph.innerText = "Added with Javascript!";
+document.body.appendChild(newParagraph);
+
+const newImage = document.createElement("img");
+newImage.setAttribute("src", "https://placeimg.com/200/200/animals");
+newImage.setAttribute("alt", "Description of image");
+document.body.appendChild(newImage);
+
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+document.body.appendChild(newDiv);
+
+const newSection = document.createElement('section')
+newSection.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>"
+
+document.body.appendChild(newSection);
