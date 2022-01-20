4d057025793e917c0f0d9501a73e96c4ebedda32
JamesonJolley <jol21004@byui.edu>
2022-01-20 03:55:06 -0700

    it works

--- W03 Tasks/week03/scripts/task3.js
+++ W03 Tasks/week03/scripts/task3.js
@@ -3,21 +3,51 @@
 /* FUNCTIONS */
 
 // Step 1: Using function declaration, define a function named add that takes two arguments, number1 and number2
+function add( number1,number2){
 
 // Step 2: In the function, return the sum of the parameters number1 and number2
-
+return Number(number1) + Number(number2)
+}
 // Step 3: Step 3: Using function declaration, define another function named addNumbers that gets the values of two HTML form controls with IDs of addend1 and addend2. Pass them to the add function
-
+function addNumbers(){
+    let append1 = document.querySelector('#addend1').value
+    let append2 = document.querySelector('#addend2').value
+ 
 // Step 4: Assign the return value to an HTML form element with an ID of sum
-
+    document.querySelector('#sum').value = add(append1,append2)
+    console.log('!')
+}    
 // Step 5: Add a "click" event listener to the HTML button with an ID of addNumbers that calls the addNumbers function
-
+document.querySelector('#addNumbers').addEventListener("click",addNumbers)
 // Step 6: Using function expressions, repeat Steps 1-5 with new functions named subtract and subtractNumbers and HTML form controls with IDs of minuend, subtrahend, difference and subtractNumbers
-
+function subtract(number1,number2){
+    return number1 - number2
+}
+function subtractNumbers(){
+    let append1 = document.querySelector('#minuend').value
+    let append2 = document.querySelector('#subtrahend').value
+    document.querySelector('#difference').value = subtract(append1,append2)
+    console.log('!')
+}
+document.querySelector('#subtractNumbers').addEventListener("click",subtractNumbers)
 // Step 7: Using arrow functions, repeat Steps 1-5 with new functions named multiply and mulitplyNumbers and HTML form controls with IDs of factor1, factor2, product and multiplyNumbers
-
+const multiply = (number1,number2) => number1*number2
+const mulitplyNumbers = () => {
+    let append1 = document.querySelector('#factor1').value
+    let append2 = document.querySelector('#factor2').value
+    document.querySelector('#product').value = multiply(append1,append2)
+    console.log('!')
+}
+document.querySelector('#multiplyNumbers').addEventListener("click",mulitplyNumbers)
 // Step 8: Using any of the three function declaration types, repeat Steps 1-5 with new functions named divide and divideNumbers and HTML form controls with IDs of dividend, divisor, quotient and divideNumbers
-
+const divide = (number1,number2) => number1/number2
+function divideNumbers(){
+    let append1 = document.querySelector('#dividend').value
+    let append2 = document.querySelector('#divisor').value
+    document.querySelector('#quotient').value = divide(append1,append2)
+    console.log('!')
+}
+document.querySelector('#divideNumbers').addEventListener("click",divideNumbers)
 // Step 9: Test all of the mathematical functionality of the task3.html page.
 
 
